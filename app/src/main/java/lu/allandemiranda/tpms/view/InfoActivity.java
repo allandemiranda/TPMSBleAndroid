b3680f48d8c6d7135f74abdce21737613bbe404d
Allan de Miranda <allandemiranda@gmail.com>
2025-07-26 00:31:15 +0200

    initial

--- app/src/main/java/lu/allandemiranda/tpms/view/InfoActivity.java
+++ app/src/main/java/lu/allandemiranda/tpms/view/InfoActivity.java
@@ -15,8 +15,8 @@ public class InfoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_info);
 
         TextView tv = findViewById(R.id.tvInfo);
-        String txt = "Static configuration:\n"
-                + "Front: " + Config.FRONT_MAC + " min " + Config.FRONT_MIN_KPA + "kPa max " + Config.FRONT_MAX_KPA + "kPa\n"
+        String txt = "Static configuration:\n\n"
+                + "Front: " + Config.FRONT_MAC + " min " + Config.FRONT_MIN_KPA + "kPa max " + Config.FRONT_MAX_KPA + "kPa\n\n"
                 + "Rear : " + Config.REAR_MAC + " min " + Config.REAR_MIN_KPA + "kPa max " + Config.REAR_MAX_KPA + "kPa";
         tv.setText(txt);
     }
